42f1446f26472b4f159fd4a52178c68169baef74
Sarthak Gupta <sarthakgupta259@gmail.com>
2017-10-18 20:36:35 +0530

    Update neuralnetwork.py

--- neuralnetwork.py
+++ neuralnetwork.py
@@ -133,7 +133,7 @@ class layer:
         self.activations = np.zeros([num_nodes_in_layer,1])
         if num_nodes_in_next_layer != 0:
             self.weights_for_layer = np.random.normal(0, 0.001, size=(num_nodes_in_layer, num_nodes_in_next_layer))
-            self.bias_for_layer = np.random.normal(0, 0.01, size=(1, num_nodes_in_next_layer))
+            self.bias_for_layer = np.random.normal(0, 0.001, size=(1, num_nodes_in_next_layer))
         else:
             self.weights_for_layer = None
             self.bias_for_layer = None
