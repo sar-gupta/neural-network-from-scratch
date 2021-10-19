f84f4fadd2d72de6f2cb2a8fcd7339f54c906618
Tejas Kannampuzha <tejaskannampuzha@gmail.com>
2021-10-19 20:11:37 +0530

    modularised code , changed line number 29 from load_training() to load_testing() ,

--- classify-mnist.py
+++ classify-mnist.py
@@ -1,3 +1,5 @@
+import sys
+
 import numpy as np
 import neuralnetwork as nn
 from mnist import MNIST
@@ -5,21 +7,37 @@ from mnist import MNIST
 print("Starting...")
 
 mndata = MNIST('mnist-dataset')
-images, labels = mndata.load_training()
 
-print("Training...")
 
-# train
 num_classes = 10
-targets = np.array([labels]).reshape(-1)
-one_hot_targets = np.eye(num_classes)[labels]
 net = nn.neural_network(3, [784, 20, 10], [None, "tanh", "softmax"], cost_function="cross_entropy")
-net.train(1, inputs=images, labels=one_hot_targets, num_epochs=5, learning_rate=0.001, filename="savepoint.pkl")
 
-print("Testing...")
+def train():
+    print("Training...")
 
-# test
-images, labels = mndata.load_testing()
-targets = np.array([labels]).reshape(-1)
-one_hot_targets = np.eye(num_classes)[labels]
-net.check_accuracy("savepoint.pkl", images, one_hot_targets)
+    # train
+    images, labels = mndata.load_training()
+    targets = np.array([labels]).reshape(-1)
+    one_hot_targets = np.eye(num_classes)[labels]
+    net.train(1, inputs=images, labels=one_hot_targets, num_epochs=1, learning_rate=0.001, filename="savepoint.pkl")
+    return()
+
+def test():
+    print("Testing...")
+
+    # test
+    images, labels = mndata.load_testing()
+    targets = np.array([labels]).reshape(-1)
+    one_hot_targets = np.eye(num_classes)[labels]
+    net.check_accuracy("savepoint.pkl", images, one_hot_targets)
+
+    return()
+
+
+if __name__=='__main__':
+    while True:
+        if input('Enter 0 for Training , 1 for testing') == '1':
+            test()
+            sys.exit()
+        else:
+            train()
