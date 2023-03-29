01512b2f96cc39f3880ae6e1c47b6202198107e4
kannampuzha <tejaskannanpuzha@gmail.com>
2023-03-30 00:34:02 +0530

    Fixed error in testing

--- classify-mnist.py
+++ classify-mnist.py
@@ -1,43 +1,51 @@
-import sys
 
 import numpy as np
-import neuralnetwork as nn
-from mnist import MNIST
+import main as nn
+#
 
 print("Starting...")
 
-mndata = MNIST('mnist-dataset')
+from mnist import MNIST
 
 
-num_classes = 10
-net = nn.neural_network(3, [784, 20, 10], [None, "tanh", "softmax"], cost_function="cross_entropy")
-
-def train():
-    print("Training...")
-
-    # train
-    images, labels = mndata.load_training()
-    targets = np.array([labels]).reshape(-1)
-    one_hot_targets = np.eye(num_classes)[labels]
-    net.train(1, inputs=images, labels=one_hot_targets, num_epochs=1, learning_rate=0.001, filename="savepoint.pkl")
-    return()
-
-def test():
+def tester(netwotk, mndatan, num_classesn):
     print("Testing...")
 
     # test
     images, labels = mndata.load_testing()
     targets = np.array([labels]).reshape(-1)
     one_hot_targets = np.eye(num_classes)[labels]
-    net.check_accuracy("savepoint.pkl", images, one_hot_targets)
+    net.check_accuracy("savepoint.pkl", images, targets)
 
-    return()
+    return ()
 
 
-if __name__=='__main__':
-    while True:
+def trainer(network, mndatan, num_classesn):
+    print("Training...")
+
+    # train
+    images, labels = mndata.load_training()
+    targets = np.array([labels]).reshape(-1)
+    one_hot_targets = np.eye(num_classes)[labels]
+    net.train(1, inputs=images, labels=one_hot_targets, num_epochs=1, learning_rate=0.001,
+              filename="savepoint.pkl")
+    return ()
+
+
+while True:
+
         if input('Enter 0 for Training , 1 for testing') == '1':
-            test()
-            sys.exit()
+            mndata = MNIST('mnist-dataset')
+
+            num_classes = 10
+            net = nn.neural_network(3, [784, 20, 10], activation_function=nn.sigmoid, cost_function=nn.cross_entropy)
+
+            tester(net, mndata, num_classes)
+            #sys.exit()
         else:
-            train()
+            mndata = MNIST('mnist-dataset')
+
+            num_classes = 10
+            net = nn.neural_network(3, [784, 20, 10], activation_function=nn.sigmoid, cost_function=nn.cross_entropy)
+
+            trainer(net,mndata,  num_classes)
